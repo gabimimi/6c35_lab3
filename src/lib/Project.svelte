ec32f442a675bd3858a399ab5d433cbf3a344a44
Gabriela Miranda <gmmirand@mit.edu>
2026-02-21 20:14:47 -0500

    retrying exercise 5.3

--- src/lib/Project.svelte
+++ src/lib/Project.svelte
@@ -1,5 +1,7 @@
 <script>
   export let data = {};
+  import projects from "$lib/projects.json";
+  import Project from "$lib/Project.svelte";
 </script>
 
 <article>
@@ -10,4 +12,12 @@
   {/if}
 
   <p>{data.description}</p>
+
+  <h2>Latest Projects</h2>
+
+    <div class="projects homepageContent">
+        {#each projects.slice(0, 3) as p}
+            <Project data={p} />
+        {/each}
+    </div>
 </article>
