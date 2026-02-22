2f1153941aafb92957941680c6d0121ceb8983a4
Gabriela Miranda <gmmirand@mit.edu>
2026-02-21 20:55:45 -0500

    sixth attempt of final draft

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,9 +21,16 @@
 <h1>Gabriela Miranda</h1>
 
 <div class="homeRow">
-  <div class="homepageContent">
-    <!-- your existing bio text + image goes here -->
-  </div>
+  <section class="homepageContent">
+        <p>Hi! I'm Gabriela Miranda, MIT '27, 6-3 (CS) + 14-1 (Econ), from Paraguay. Right now 
+            I'm at J-PAL building an NLP-powered search tool that makes it easier to find the right 
+            researchers + work without digging forever. I'm there because I care about real impact: research that 
+            actually turns into better decisions for real people. I also TA Math for CS, so I spend a lot of 
+            time making fundamentals feel less scary. Off-screen, I play piano and sing!🎹</p>
+
+        <!-- <img> (Image) tag is used to display images.-->
+        <img src="images/gabi.jpg" alt="a pic of myself... hopefully it loads soon!">
+    </section>
 
   <section class="readingPanel">
     <h2>What I’m Reading</h2>
