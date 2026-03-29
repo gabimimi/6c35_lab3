3068a817c45273a8b581ee43cfeeba0c95733056
Gabriela Miranda <gmmirand@mit.edu>
2026-03-28 20:25:42 -0400

    step 1.4

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -4,7 +4,11 @@
   import * as d3 from 'd3';
   import BarHorizontal from '$lib/BarHorizontal.svelte';
 
+  let width = 1000;
+  let height = 600;
+
   let locData = [];
+  let commits = [];
 
   onMount(async () => {
     locData = await d3.csv(`${base}/loc.csv`, row => ({
@@ -15,7 +19,37 @@
       date: new Date(row.date + 'T00:00' + row.timezone),
       datetime: new Date(row.datetime)
     }));
+
+    commits = d3.groups(locData, d => d.commit).map(([commit, lines]) => {
+      const first = lines[0];
+      const { author, date, time, timezone, datetime } = first;
+      return {
+        id: commit,
+        url: 'https://github.com/vis-society/lab-7/commit/' + commit,
+        author,
+        date,
+        time,
+        timezone,
+        datetime,
+        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+        totalLines: lines.length,
+        lines
+      };
+    });
   });
+
+  // Thanks to Nathanael Jenkins for flagging this to us!
+  $: [minDate, maxDate] = d3.extent(commits.map(d => d.date));
+  $: maxDatePlusOne = new Date(maxDate);
+  $: maxDatePlusOne.setDate(maxDatePlusOne.getDate() + 1);
+
+  $: xScale = d3
+    .scaleTime()
+    .domain([minDate, maxDatePlusOne])
+    .range([0, width])
+    .nice();
+
+  $: yScale = d3.scaleLinear().domain([24, 0]).range([height, 0]);
 
   $: languageData = d3.rollups(
     locData,
@@ -35,4 +69,25 @@
 
 <h1>Meta</h1>
 
+<h3>Commits by time of day</h3>
+
+<svg viewBox="0 0 {width} {height}">
+  <g class="dots">
+    {#each commits as commit, index}
+      <circle
+        cx={xScale(commit.datetime)}
+        cy={yScale(commit.hourFrac)}
+        r="5"
+        fill="steelblue"
+      />
+    {/each}
+  </g>
+</svg>
+
 <BarHorizontal data={languageData} title="Lines of Code by Language" />
+
+<style>
+  svg {
+    overflow: visible;
+  }
+</style>
